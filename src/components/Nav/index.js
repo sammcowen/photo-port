679254b59e6de6ac2b523e36559c8b7a5f6c9961
HBK <sammcowen@gmail.com>
2022-05-03 19:50:21 -0700

    almost done with lesson1

---
+++ src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+function Nav() {
+    return (
+
+        <header>
+            <h2>
+            <a href="/">
+                <span role='img' aria-label='camera'>📷</span>Oh Snap1
+            </a>
+            </h2>
+            <nav>
+                <ul className='flex-row'>
+                    <li className="mx-2">
+                        <a href ="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+    );
+}
+
+export default Nav;
